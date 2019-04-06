e6bf5f3aaed71930e17a4211d665b25b5a3ab867
aplJake <apl.jake@gmail.com>
2019-04-06 18:46:29 +0300

    [added] writing test links for all years frm 1 sub

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-MAIN_URL = "https://zno.osvita.ua/ukrainian/"
+WEBSITE_URL = "https://zno.osvita.ua"
+MAIN_URL = "https://zno.osvita.ua/ukrainian"
+page_tests_urls = []
+
 
 r = requests.get(MAIN_URL)
 
 def parse_soup():
     soup = BeautifulSoup(r.content, 'html.parser')
-    print(soup)
+    # print(soup)
+
+    test_blocks = soup.select(".col-main .tests-block")
+    # print(test_blocks)
+
+    # getting urls from a tag
+    #  from ul-test blocks
+    for row in test_blocks:
+        link = row.select_one(".test-item a")["href"]
+        complete_link = WEBSITE_URL + link
+        page_tests_urls.append(complete_link)
 
 
 def save_html(html, path):
