717f9830ffa5e02e034d6c88e5c096bf91507e15
aplJake <apl.jake@gmail.com>
2019-04-06 19:09:32 +0300

    [added] controller file

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+from subject_page_parse import SingleSubjectParse
 
+# constants
 WEBSITE_URL = "https://zno.osvita.ua"
-MAIN_URL = "https://zno.osvita.ua/ukrainian"
-page_tests_urls = []
-
-
-r = requests.get(MAIN_URL)
-
-def parse_soup():
-    soup = BeautifulSoup(r.content, 'html.parser')
-    # print(soup)
-
-    test_blocks = soup.select(".col-main .tests-block")
-    # print(test_blocks)
-
-    # getting urls from a tag
-    #  from ul-test blocks
-    for row in test_blocks:
-        link = row.select_one(".test-item a")["href"]
-        complete_link = WEBSITE_URL + link
-        page_tests_urls.append(complete_link)
-
-
-def save_html(html, path):
-    with open(path, "wb") as f:
-        f.write(html)
-
+SINGLE_SUBJECT_URL = "https://zno.osvita.ua/ukrainian"
 
 if __name__ == "__main__":
-    parse_soup()
+    # create single subject test-link array
+    subjectParse = SingleSubjectParse(SINGLE_SUBJECT_URL, WEBSITE_URL)
+    z_subject_tests = subjectParse.parse_soup().test_urls
+    print(z_subject_tests)
